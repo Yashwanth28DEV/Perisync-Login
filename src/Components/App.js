7a23257876025edf39f2ec449b0a7975f7415b86
Yashwanth <yashwanthyashu8098@GMAIL.COM>
2023-08-24 13:58:28 +0530

    second commit

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,26 +1,22 @@
 import React, { useState } from "react";
-// import Maincomponent from "./Redux/Maincomponent";
-// import store from "./Redux/Store";
-// import {Provider} from 'react-redux'
-// import Cakehookcomp from "./Redux/cake/Cakehookcomp";
-// import Icecreamhookcomp from "./Redux/Icecream/Icecreamhookcomp";
+
 import Home from "./Sampleproject/Home"
 
-let App=()=>{
+ let App=()=>{
      
     
      
     return(
-        // <Provider store={store}>
+        
              <div>
                <Home/>
             </div>
-        // </Provider>
+        
         
         
 
     )
 }
-// export default usercontext
+
 
 export default  App
